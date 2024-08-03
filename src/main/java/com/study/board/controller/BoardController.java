3fe0962b41f1d5c142b5b7f8cee634c1df6fc64e
김가영 <gyk7@naver.com>
2024-08-03 11:50:19 +0900

    페이징 처리, 검색기능 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -31,9 +35,26 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model) {
+    public String boardList(Model model,
+                            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC)Pageable pageable,
+                            @RequestParam(name = "searchKeyword", defaultValue = "") String searchKeyword) {
 
-        model.addAttribute("List", boardService.boardList());
+        Page<Board> list = null;
+
+        if(searchKeyword == null) {
+            list = boardService.boardList(pageable);
+        }else {
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        model.addAttribute("List", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
 
         return "boardList";
     }
@@ -51,13 +72,15 @@ public class BoardController {
     }
     @GetMapping("/board/modify/{id}")
     public String boardModify(@PathVariable("id") Integer id,
-                              Model model) {
+                              Model model, @RequestParam(name="file", required = false) MultipartFile file){
+
         model.addAttribute("board", boardService.boardView(id));
+
         return "boardmodify";
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board, @RequestParam(name="file", required = false) MultipartFile file) throws Exception{
+    public String boardUpdate(@PathVariable("id") Integer id, Model model, Board board, @RequestParam(name="file", required = false) MultipartFile file) throws Exception{
         Board boardTemp = boardService.boardView(id);
 
         boardTemp.setTitle(board.getTitle());
@@ -65,8 +88,19 @@ public class BoardController {
 
         boardService.write(boardTemp, file);
 
-        return "redirect:/board/list";
+        if (file != null && !file.isEmpty()) {
+            boardService.write(boardTemp, file);
+        } else {
+            boardService.updateWithoutFile(boardTemp);
+        }
+
+        model.addAttribute("message", "글 수정이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
+//        return "redirect:/board/list";
     }
+    
 
 
 
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,31 +21,43 @@ public class BoardService {
     // 글 작성 처리
     public void write(Board board, MultipartFile file) throws Exception {
 
-        String projectPath = System.getProperty("user.dir") +"\\src\\main\\resources\\static\\files"; // 프로젝트 경로, 파일경로 담아줌.
+        if (file != null && !file.isEmpty()) {
+            String projectPath = System.getProperty("user.dir") +"\\src\\main\\resources\\static\\files"; // 프로젝트 경로, 파일경로 담아줌.
 
-        UUID uuid = UUID.randomUUID();  // 랜덤으로 이름 만들어줌.
+            UUID uuid = UUID.randomUUID();  // 랜덤으로 이름 만들어줌.
 
-        String fileName = uuid + "_" + file.getOriginalFilename();
+            String fileName = uuid + "_" + file.getOriginalFilename();
 
-        File saveFile = new File(projectPath, fileName); // projectPath에 fileName을 가진 파일을 만들어줌.
+            File saveFile = new File(projectPath, fileName); // projectPath에 fileName을 가진 파일을 만들어줌.
 
-        file.transferTo(saveFile);
+            file.transferTo(saveFile);
 
-        board.setFilename(fileName);
-        board.setFilepath("/files/" + fileName);
+            board.setFilename(fileName);
+            board.setFilepath("/files/" + fileName);
+        }
 
         boardRepository.save(board);
     }
 
-    // 게시글 리스트 처리
-    public List<Board> boardList() {
-        return boardRepository.findAll();
+    // 파일 없이 글 수정 처리
+    public void updateWithoutFile(Board board) {
+        boardRepository.save(board);
     }
 
+
+    // 게시글 리스트 처리
+    public Page<Board> boardList(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
+
+    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable) {
+        return boardRepository.findByTitleContaining(searchKeyword, pageable);
+    }
     // 특정 게시글 불러오기
     public Board boardView(Integer id){
         return boardRepository.findById(id).get();
     }
+
 
     // 특정 게시글 삭제
     public void boardDelete(Integer id) {
